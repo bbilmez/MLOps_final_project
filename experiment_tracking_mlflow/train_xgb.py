06f2c4a6131135fc017a3ab7628ddfcb70c90d62
Banu Bilmez <b.bilmez@gerimedica.nl>
2023-07-25 17:23:13 +0200

    Changed the model

--- experiment_tracking_mlflow/train_xgb.py
+++ experiment_tracking_mlflow/train_xgb.py
@@ -3,8 +3,8 @@ import os
 import pickle
 import click
 import mlflow
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error
+import xgboost as xgb
+from sklearn.metrics import accuracy_score
 
 mlflow.set_tracking_uri("http://127.0.0.1:5000")
 mlflow.set_experiment("heart_disease_experiment")
@@ -17,7 +17,7 @@ def load_pickle(filename: str):
 @click.command()
 @click.option(
     "--data_path",
-    default="./Output",
+    default="../Output",
     help="Location where the heart disease data was saved"
 )
 
@@ -28,21 +28,34 @@ def run_train(data_path: str):
         dv_path = os.path.join(data_path, "dv.pkl")
         dv = load_pickle(dv_path)
 
-        max_depth = 10
-        rf = RandomForestRegressor(max_depth=max_depth, random_state=0)
-        rf.fit(X_train, y_train)
-        y_pred = rf.predict(X_val)
+        params = {
+            'max_depth': 7,
+            'learning_rate': 0.05,
+            'n_estimators': 77,
+            'min_child_weight': 7,
+            'gamma': 0.5,
+            'subsample': 0.5,
+            'colsample_bytree': 0.5,
+            'reg_alpha': 0.05,
+            'reg_lambda': 1e-7,
+            'eval_metric': 'mlogloss',
+            'tree_method': 'approx', 
+            'eval_metric' : 'logloss',
+        }
+        xgb_model = xgb.XGBClassifier(**params)
+        xgb_model.fit(X_train, y_train)
+        y_pred = xgb_model.predict(X_val)
 
-        rmse = mean_squared_error(y_val, y_pred, squared=False)
+        accuracy = accuracy_score(y_val, y_pred)
         
         with open(dv_path, 'wb') as f_out:
             pickle.dump(dv, f_out)
         mlflow.log_artifact(dv_path, artifact_path="dict_vectorizer")
 
 
-        mlflow.log_param("max_depth", max_depth)
-        mlflow.log_metric("rmse", rmse)
-        mlflow.sklearn.log_model(rf, "model")
+        mlflow.log_param("parameters", params)
+        mlflow.log_metric("accuracy", accuracy)
+        mlflow.sklearn.log_model(xgb_model, "model")
 
 
 if __name__ == '__main__':
